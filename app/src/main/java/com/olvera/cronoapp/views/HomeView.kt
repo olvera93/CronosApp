9c7a60bc129fb8d17cc861aee89188a5972d7be2
olvera93 <olvera9302@outlook.com>
2024-01-08 11:25:54 -0600

    [ADD] Swhipe for deleting an item

--- app/src/main/java/com/olvera/cronoapp/views/HomeView.kt
+++ app/src/main/java/com/olvera/cronoapp/views/HomeView.kt
@@ -5,23 +5,28 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.olvera.cronoapp.components.ChronCard
 import com.olvera.cronoapp.components.FloatButton
 import com.olvera.cronoapp.components.MainTitle
 import com.olvera.cronoapp.components.timeFormat
-import com.olvera.cronoapp.viewModel.ChronometerViewModel
 import com.olvera.cronoapp.viewModel.CronosViewModel
+import me.saket.swipe.SwipeAction
+import me.saket.swipe.SwipeableActionsBox
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -64,12 +69,25 @@ fun ContentHomeView(
 
         LazyColumn() {
             items(chronosList) { item ->
-                ChronCard(
-                    title = item.title,
-                    chrono = timeFormat(item.crono)
-                ) {}
+
+                val delete = SwipeAction(
+                    icon = rememberVectorPainter(Icons.Default.Delete),
+                    background = Color.Red,
+                    onSwipe = { chronometerViewModel.deleteCrono(item) }
+                )
+
+                SwipeableActionsBox(
+                    endActions = listOf(delete),
+                    swipeThreshold = 270.dp
+                ) {
+                    ChronCard(
+                        title = item.title,
+                        chrono = timeFormat(item.crono)
+                    ) {
+
+                    }
+                }
             }
         }
-
     }
 }
